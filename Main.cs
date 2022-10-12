a75c9599017c3d54afe6e579f8b9a61fdf9f91d5
xyrulo <75859018+xyrulo@users.noreply.github.com>
2022-10-11 19:36:06 -0700

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+    static public Main S;
+
+    public GameObject[] prefabEnemies;
+    public float enemySpawnPerSecond = 0.5f;
+    public float enemySpawnPadding = 1.5f;
+    public float enemySpawnRate;
+    public int enemyRange = prefabEnemies.length;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        S = this;
+        enemySpawnRate = 1f/enemySpawnPerSecond;
+        Invoke("SpawnEnemy", enemySpawnRate);
+    }
+
+    void SpawnEnemy() {
+        // Pick random enemy, increase difficulty of enemies as game continues
+        int ndx = Random.Range(0, enemyRange);
+        GameObject go = Instantiate(prefabEnemies[ndx]);
+        Vector3 pos = Vector3.zero;
+        float xMin = Utils.camBounds.min.x + enemySpawnPadding;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
